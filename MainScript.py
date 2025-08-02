7adfe5e84a3f26adb7484fd6e381e833d1c53f4f
qkhadim22 <qasim.lut@icloud.com>
2025-08-02 13:23:39 +0300

    MainScript

---
+++ MainScript.py
@@ -0,0 +1,40 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Author:   Qasim Khadim and Johannes Gerstmayr
+# Contact : qasim.khadim@outlook.com,qkhadim22 (Github)
+# Date:     2025-01-08
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from Models.Container import *
+
+###############################
+
+Patu            = False
+timeStep        =  1e-3                  #Simulation time step: Change it as desired.
+T               =  10                   #Time period
+ns              = int(T/timeStep)       
+angleInit1      = np.deg2rad(14.6)      #LiftBoom anglee              
+angleInit2      = np.deg2rad(-58.8)     #TiltBoom angle 
+LiftLoad        = 0              #469.1387
+
+#in this section we create or load data
+if  Patu:
+    dataFile        = 'solution/TwoArms/'+str(T) + '-' + 's' + str(ns) + 'Steps' + str(LiftLoad)+'Load'
+else: 
+    dataFile        = 'solution/OneArm/'+str(T) + '-' + 's' + str(ns) + 'Steps' + str(LiftLoad)+'Load'
+    
+
+model       = NNHydraulics(nStepsTotal=ns, endTime=T,  mL    = LiftLoad,Flexible=True, 
+                           nModes=50, loadFromSavedNPY=False, system=Patu, verboseMode=1)
+
+inputVec    = model.CreateInputVector( ns,  angleInit1,angleInit2,system=Patu )
+
+
+data = model.ComputeModel(inputVec,system=Patu,  solutionViewer = True) #solutionViewer: for visualization
+
+data_array = np.array(data, dtype=object)
+np.save(dataFile, data_array)
+
+Plotting    =  True
+
+if Plotting:
+   model.Plotting(data_array)
